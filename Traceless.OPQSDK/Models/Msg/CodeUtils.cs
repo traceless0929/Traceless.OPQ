37e87e7ca072486c51f13d2f1f4609891e92a03b
Traceless <Traceless0929@outlook.com>
2020-09-05 18:49:18 +0800

    增加文本宏

--- Traceless.OPQSDK/Models/Msg/CodeUtils.cs
+++ Traceless.OPQSDK/Models/Msg/CodeUtils.cs
@@ -6,6 +6,11 @@ namespace Traceless.OPQSDK.Models.Msg
 {
     public class CodeUtils
     {
+        /// <summary>
+        /// 获取ATCode，可以实现At列表中的QQ
+        /// </summary>
+        /// <param name="atList"></param>
+        /// <returns></returns>
         public static string At(params long[] atList)
         {
             if (atList == null || atList.Length < 1)
