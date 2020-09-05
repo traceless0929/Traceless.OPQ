32d5ff0099a8277b7b0834b1edffbafda096bebd
Traceless <Traceless0929@outlook.com>
2020-09-05 21:10:41 +0800

    增加文本宏 获取昵称

--- Traceless.OPQSDK/Models/Msg/CodeUtils.cs
+++ Traceless.OPQSDK/Models/Msg/CodeUtils.cs
@@ -9,7 +9,7 @@ namespace Traceless.OPQSDK.Models.Msg
         /// <summary>
         /// 获取ATCode，可以实现At列表中的QQ
         /// </summary>
-        /// <param name="atList"></param>
+        /// <param name="atList">传一个-1表示At全体成员</param>
         /// <returns></returns>
         public static string At(params long[] atList)
         {
@@ -17,7 +17,20 @@ namespace Traceless.OPQSDK.Models.Msg
             {
                 return "";
             }
+            if(atList.Length==1&&atList[0]==-1){
+                return $"[ATALL()]";
+            }
             return $"[ATUSER({string.Join(",",atList)})]";
+        }
+
+        /// <summary>
+        /// 获取昵称
+        /// </summary>
+        /// <param name="qq">QQ号</param>
+        /// <returns></returns>
+        public static string UserNick(long qq)
+        {
+            return $"[GETUSERNICK({qq})]";
         }
     }
 }
