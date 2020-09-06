12dc8e8832381f28a776bbc6556748a26e1eefb8
Traceless <Traceless0929@outlook.com>
2020-09-06 14:18:04 +0800

    fix

--- Traceless.OPQSDK/Apis.cs
+++ Traceless.OPQSDK/Apis.cs
@@ -287,17 +287,19 @@ namespace Traceless.OPQSDK
         private static MsgResp SendMsg(SendMsgReq req)
         {
             MsgResp msg = Post<MsgResp>(_ApiAddress + "&funcname=SendMsg", req);
-            //int i=0;
-            //while(msg.Ret==241&&i<5){
-            //    Console.WriteLine($"[WARN]API等待{JsonConvert.SerializeObject(req)}");
-            //    System.Threading.Thread.Sleep(1100);
-            //    msg = Post<MsgResp>(_ApiAddress + "&funcname=SendMsg", req);
-            //    i++;
-            //}
-            //if(msg.Ret==241){
-                
-            //    Console.WriteLine($"[WARN]API调用过于频繁，本条丢弃{JsonConvert.SerializeObject(req)}");
-            //}
+            int i = 0;
+            while (msg.Ret == 241 && i < 10)
+            {
+                Console.WriteLine($"[WARN]API等待{JsonConvert.SerializeObject(req)}");
+                System.Threading.Thread.Sleep(1100);
+                msg = Post<MsgResp>(_ApiAddress + "&funcname=SendMsg", req);
+                i++;
+            }
+            if (msg.Ret == 241)
+            {
+
+                Console.WriteLine($"[WARN]API调用过于频繁，本条丢弃{JsonConvert.SerializeObject(req)}");
+            }
             return msg;
         }
 
